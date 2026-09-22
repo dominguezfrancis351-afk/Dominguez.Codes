66e6140ad0e82426395d846ab7809747ad5d667f
dominguezfrancis351-afk <dominguezfrancis351@gmail.com>
2026-09-22 23:21:21 +0800

    gcdDomz.java

--- gcdDomz.java
+++ gcdDomz.java
@@ -1,1 +1,38 @@
+import java.util.Scanner;
 
+public class RecursiveGCD {
+
+    public static int gcd(int a, int b) {
+        System.out.println("gcd(" + a + ", " + b + ")");
+
+        if (b == 0) {
+            return a;
+        }
+
+        System.out.println("a % b = " + (a % b));
+
+        return gcd(b, a % b);
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = input.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = input.nextInt();
+
+        if (a == 0 && b == 0) {
+            System.out.println("The two numbers cannot both be zero.");
+        } else if (a < 0 || b < 0) {
+            System.out.println("Please enter non-negative integers.");
+        } else {
+            int result = gcd(a, b);
+
+            System.out.println("GCD = " + result);
+        }
+
+        input.close();
+    }
+}
